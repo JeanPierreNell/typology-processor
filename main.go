850435b52044fd7878e6c2e222b6ce506c94174b
Jean-Pierre Nell <JeanPierreNell997@gmail.com>
2023-12-01 12:10:01 +0200

    feat: removed double nats connection

--- main.go
+++ main.go
@@ -1,55 +1,28 @@
 package main
 
 import (
-	"log"
-	"os"
 	"runtime"
 
 	db "typology-processor/db-lib"
-	P "typology-processor/proto"
 
 	"github.com/joho/godotenv"
-	"github.com/nats-io/nats.go"
-	"google.golang.org/protobuf/proto"
 )
 
 // Client
 
 func main() {
 
-	godotenv.Load()
-	subject := os.Getenv("CONSUMER_STREAM")
+	err := godotenv.Load()
 
-	// Create server connection
-	//nats.DefaultURL
-	//nats://127.0.0.1:14222
-	natsConnection, natsError := nats.Connect(os.Getenv("SERVER_URL"))
-
-	if natsError != nil {
-		log.Fatal("Could not connect to NATS")
-		panic("Could not connect to NATS")
+	if err != nil {
+		println(err)
 	}
 
-	log.Println("Connected to NATS on: " + os.Getenv("SERVER_URL"))
-
+	// Init Arango DB
 	db.InitDatabases()
 
-	// Subscribe to subject & Handle incoming messages.
-	natsConnection.Subscribe(subject, func(msg *nats.Msg) {
-		log.Println("Recieved Message. Processing...")
-
-		message := &P.FRMSMessage{}
-		err := proto.Unmarshal(msg.Data, message)
-
-		if err != nil {
-			log.Fatal("Could not unmarshal Protobuff object.")
-		}
-
-		// HandleTransaction(string(msg.Data))
-		HandleTransaction(message)
-
-		log.Println("Message Resolved.")
-	})
+	// Init NATS Subscription
+	Subscribe(HandleTransaction)
 
 	// Keep the connection alive
 	runtime.Goexit()
--- nats.service.go
+++ nats.service.go
@@ -11,23 +11,52 @@ import (
 	"github.com/nats-io/nats.go"
 )
 
+type natsFunction func(*P.FRMSMessage)
+
 var natsConnection *nats.Conn
 
-func Subscribe() {
+func Subscribe(function natsFunction) {
+
+	subject := os.Getenv("CONSUMER_STREAM")
+	serverUrl := os.Getenv("SERVER_URL")
+	var natsError error
+
+	natsConnection, natsError = nats.Connect(serverUrl)
+
+	if natsError != nil {
+		log.Fatal("Could not connect to NATS")
+		panic("Could not connect to NATS")
+	}
+
+	log.Println("Connected to NATS on: " + serverUrl)
+
+	natsConnection.Subscribe(subject, func(msg *nats.Msg) {
+		log.Println("Recieved Message. Processing...")
+
+		message := &P.FRMSMessage{}
+		err := proto.Unmarshal(msg.Data, message)
+
+		if err != nil {
+			log.Fatal("Could not unmarshal Protobuff object.")
+		}
+
+		function(message)
+		log.Println("Message Resolved.")
+	})
 
 }
 
 func HandleResponse(data *P.FRMSMessage) {
-	natsConnection, _ = nats.Connect(os.Getenv("SERVER_URL"))
-	log.Println("Connected to " + nats.DefaultURL)
+	producerStream := os.Getenv("PRODUCER_STREAM")
 
 	log.Println("Sending Message...")
-	// Publish message on subject
-	// message := json.Unmarshal([]byte(data), string)
-	// message, _ := json.Marshal(data)
-
 	message, _ := proto.Marshal(data)
 
-	natsConnection.Publish(os.Getenv("PRODUCER_STREAM"), message)
-	log.Println("Message Send.")
+	err := natsConnection.Publish(producerStream, message)
+
+	if err != nil {
+		log.Println("Failed to send message to NATS" + err.Error())
+	} else {
+		log.Println("Message Send.")
+	}
 }
